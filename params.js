1c8c529807752417d53d1faa3a99ea3fadd72a44
Andrey Gershun <agershun@agmacpro.local>
2018-09-16 14:03:52 +0500

    First Commit

---
+++ params.js
@@ -0,0 +1,11 @@
+let params = {};
+
+if (location.search) {
+    let parts = location.search.substring(1).split('&');
+
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+}
